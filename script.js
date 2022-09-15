7b419d56f316b6dc1adfdcd5210fc455a6a644c8
electricman07 <63125433+electricman07@users.noreply.github.com>
2022-09-15 09:45:12 -0700

    added listeners and classes for javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+const number1 = document.querySelector(".number1");
+const number2 = document.querySelector(".number2");
+const number3 = document.querySelector(".number3");
+const number4 = document.querySelector(".number4");
+const number5 = document.querySelector(".number5");
+const btn = document.querySelector(".btn");
+
+const clicked = function () {};
