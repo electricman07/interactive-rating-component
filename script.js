5052ec3cd06f3db3344b6af2a7204d8dba048f3a
electricman07 <63125433+electricman07@users.noreply.github.com>
2022-09-22 14:25:22 -0700

    commented out some code

--- script.js
+++ script.js
@@ -3,6 +3,23 @@ const number2 = document.querySelector(".number2");
 const number3 = document.querySelector(".number3");
 const number4 = document.querySelector(".number4");
 const number5 = document.querySelector(".number5");
+const card = document.querySelector(".rating-flex-container");
 const btn = document.querySelector(".btn");
 
-const clicked = function () {};
+const star = function (x) {
+  // const x = this.value;
+  console.log(x);
+  // set active state for star
+  //   number1.classList.add("active");
+  //   console.log();
+};
+
+const clicked = function () {
+  // check if star selected (active state)
+  // identify which star selected
+  // hide screen 1
+  // unhide thank you card
+  // put selected number in selected box
+};
+
+card.addEventListener("click", star);
